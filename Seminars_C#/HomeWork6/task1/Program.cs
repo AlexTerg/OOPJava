58451db10de10a5a9bad5f3b2c6e3db48c2633e7
AlexTerg <s.tergoev@gmail.com>
2022-09-18 12:29:02 +0300

    homework for the sixth seminar

---
+++ Seminars_C#/HomeWork6/task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Было несколько вариантов решения, оставил два
+
+Console.WriteLine("Введите числа через запятую\t");
+int[] NumbersArray = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+int FindNumbersMoreZero(int[] array)
+{
+    int cnt = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            cnt ++;
+        }
+    }
+    return cnt;
+}
+
+Console.WriteLine(FindNumbersMoreZero(NumbersArray));
+
+// Еще один вариант решения.
+
+// Console.WriteLine("Введите количество чисел\t");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int FindNumbersMoreZero(int arg)
+// {
+//     int cnt = 0;
+//     for (int i = 0; i < arg; i++)
+//     {
+//         Console.WriteLine("Введите число\t");
+//         int number = Convert.ToInt32(Console.ReadLine());
+//         if (number > 0)
+//         {
+//             cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+
+// Console.WriteLine(FindNumbersMoreZero(size));
---
+++ Seminars_C#/HomeWork6/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение b1\t");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k1\t");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2\t");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2\t");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+// k1 * x + b1 = k2 * x + b2
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2) 
+
+double FindY(double b1, double k1, double b2, double k2)
+{
+   double x = (b2 - b1) / (k1 - k2);
+   return k1 * x + b1;
+}
+
+double y = FindY(b1, k1, b2, k2);
+Console.WriteLine($"({y};{y})");
