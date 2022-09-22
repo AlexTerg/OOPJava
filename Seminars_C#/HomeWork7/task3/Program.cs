5a8c11e823131bf34f5432ca67e3e3c282941c69
AlexTerg <s.tergoev@gmail.com>
2022-09-22 10:19:52 +0300

    corrected task3

--- Seminars_C#/HomeWork7/task3/Program.cs
+++ Seminars_C#/HomeWork7/task3/Program.cs
@@ -9,14 +9,14 @@ Average(array);
 void Average(int[,] array)
 {
     
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(1); i++)
     {   
         double sum = 0;
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(0); j++)
         {
-            sum += array[i,j];
+            sum += array[j, i];
         }
-        Console.Write($"{sum / array.GetLength(1)} ");
+        Console.Write($"{sum / array.GetLength(0)} ");
     }    
 }
 
